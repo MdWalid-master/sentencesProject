bc53781ab94eab57efc1eddacf503fd024bead7e
mdwalidbz <mdwalidbz@gmail.com>
2021-12-07 12:39:56 +0100

    refactor table associative

--- generatorRNA.py
+++ generatorRNA.py
@@ -4,16 +4,17 @@ import pandas as pd
 
 
 #trasformation de la table associative sous forme d'un dictionnaire {'POS': [liste de mots]}
-dicTableAssociative = {}
-with open("data/TableAssociative.txt", "r", encoding="utf-8") as f:
-    for line in f.readlines():
-        lineContent = list(filter(('').__ne__,re.split("\t|\n",line)))
-        dicTableAssociative[lineContent[0]] = lineContent[1:]
+def function_get_table_associative_csv():
+    dicTableAssociative = {}
+    with open("data/TableAssociative.txt", "r", encoding="utf-8") as f:
+        for line in f.readlines():
+            lineContent = list(filter(('').__ne__,re.split("\t|\n",line)))
+            dicTableAssociative[lineContent[0]] = lineContent[1:]
 
-
-# trasformation de la table associative sous forme d'un dataFrame qui sera stocké sous forme d'un fichier CSV
-dataframe = pd.concat({k: pd.Series(v) for k, v in dicTableAssociative.items()}, axis=1)
-dataframe.to_csv("data/TableAssociativeDict.csv", sep='\t', encoding='utf-8', index=False)
+    # trasformation de la table associative sous forme d'un dataFrame qui sera stocké sous forme d'un fichier CSV
+    dataframe = pd.concat({k: pd.Series(v) for k, v in dicTableAssociative.items()}, axis=1)
+    #dataframe.to_csv("data/TableAssociativeDict.csv", sep='\t', encoding='utf-8', index=False)
+    return dataframe
 
 
 
