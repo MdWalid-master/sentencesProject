6446e230b324147218bffbffa9287560d55e6af9
ramdane.merakeb <ramdane.merakeb@alumni.univ-avignon.fr>
2021-12-07 13:25:20 +0100

    refactoring

--- generatorRNA.py
+++ generatorRNA.py
@@ -18,10 +18,12 @@ def function_get_embbeding():
     dic_embedding = {}
     with open("data/embeddings-Fr.txt", "r", encoding="utf-8") as f:
         for line in f.readlines():
-            l = re.findall("-*\w+\.*\d*e*-*\+*\d*", line)
+            l = list(filter(('').__ne__,re.split(" |,|\[|\]|\t|\n", line)))
             dic_embedding[l[0]] = l[1:]
 
     data_embedding = pd.DataFrame(dic_embedding).astype('float64')
-    data_embedding.to_csv("data/embeddingsDict.csv", sep='\t', encoding='utf-8', index=False)
+    #data_embedding.to_csv("data/embeddingsDict.csv", sep='\t', encoding='utf-8', index=False)
 
     return data_embedding
+
+
